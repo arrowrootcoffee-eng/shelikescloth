223ce19dcb5f35f18ef043dcdd955af63622715b
arrowrootcoffee-eng <arrowrootcoffee@gmail.com>
2025-08-29 10:52:21 -0500

    Create app.tsx

---
+++ src/app.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import BandNameRater from "./BandNameRater";
+
+export default function App() {
+  return (
+    <div className="max-w-3xl mx-auto p-6">
+      <BandNameRater />
+    </div>
+  );
+}
